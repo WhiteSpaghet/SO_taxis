ca00870bef79e795225ce6f6985e9c5fda5ffbb5
WhiteSpaghet <superivasca@gmail.com>
2025-12-16 01:14:05 +0100

    a

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 // La línea del CSS ya no está aquí
 
 ReactDOM.createRoot(document.getElementById('root')).render(
